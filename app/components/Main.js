f9e956107b1f3b3b761b0ec091bfbaed32e851d0
AndyEm <andyem64@gmail.com>
2017-06-11 18:21:54 -0400

    converted to ES6

--- app/components/Main.js
+++ app/components/Main.js
@@ -1,103 +1,116 @@
 // Include React
-var React = require("react");
+// var React = require("react");
+import React from "react";
 
 // Here we include all of the sub-components
-var Form = require("./children/Form");
-var Results = require("./children/Results");
-var History = require("./children/History");
+// var Form = require("./children/Form");
+// var Results = require("./children/Results");
+// var History = require("./children/History");
+
+import Form from "./children/Form";
+import Results from "./children/Results";
+import History from "./children/History";
 
 // Helper for making AJAX requests to our API
-var helpers = require("./utils/helpers");
+// var helpers = require("./utils/helpers");
+import helpers from "./utils/helpers";
 
 // Creating the Main component
-var Main = React.createClass({
+// var Main = React.createClass
+class Main extends React.Component{
 
-  // Here we set a generic state associated with the number of clicks
-  // Note how we added in this history state variable
-  getInitialState: function() {
-      return { searchTerm: "",
-               results: "",
-               history: [] };
-  },
+    // This is the equivalent of our "getInitialState"
+    constructor(props){
+        // This super(props) line lets us access our parents properties as props.
+        super(props);
 
-  // The moment the page renders get the History
-  componentDidMount: function() {
-    // Get the latest history.
-    helpers.getHistory().then(function(response) {
-      console.log(response);
-      if (response !== this.state.history) {
-        console.log("History", response.data);
-        this.setState({ history: response.data });
-      }
-    }.bind(this));
-  },
+        this.state = {
+            searchTerm: "",
+            results: "",
+            history: []
+        };
 
-  // If the component changes (i.e. if a search is entered)...
-  componentDidUpdate: function() {
+        this.setTerm = this.setTerm.bind(this);
+    }
 
-    // Run the query for the address
-    helpers.runQuery(this.state.searchTerm).then(function(data) {
-      if (data !== this.state.results) {
-        console.log("Address", data);
-        this.setState({ results: data });
-
-        // After we've received the result... then post the search term to our history.
-        helpers.postHistory(this.state.searchTerm).then(function() {
-          console.log("Updated!");
-
-          // After we've done the post... then get the updated history
-          helpers.getHistory().then(function(response) {
-            console.log("Current History", response.data);
-
-            console.log("History", response.data);
-
-            this.setState({ history: response.data });
-
-          }.bind(this));
+    // The moment the page renders get the History
+    componentDidMount() {
+        // Get the latest history.
+        helpers.getHistory().then(function (response) {
+            console.log(response);
+            if (response !== this.state.history) {
+                console.log("History", response.data);
+                this.setState({history: response.data});
+            }
         }.bind(this));
-      }
-    }.bind(this));
-  },
-  // This function allows childrens to update the parent.
-  setTerm: function(term) {
-    this.setState({ searchTerm: term });
-  },
-  // Here we render the function
-  render: function() {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="jumbotron">
-            <h2 className="text-center">Ny Time Article Scrubber</h2>
-            <p className="text-center">
-              <em>Search for article of interest</em>
-            </p>
-          </div>
+    }
 
-          <div className="col-md-6">
+    // If the component changes (i.e. if a search is entered)...
+    componentDidUpdate(){
 
-            <Form setTerm={this.setTerm} />
+        // Run the query for the address
+        helpers.runQuery(this.state.searchTerm).then(function (data) {
+            if (data !== this.state.results) {
+                console.log("Address", data);
+                this.setState({results: data});
 
-          </div>
+                // After we've received the result... then post the search term to our history.
+                helpers.postHistory(this.state.searchTerm).then(function () {
+                    console.log("Updated!");
 
-          <div className="col-md-6">
+                    // After we've done the post... then get the updated history
+                    helpers.getHistory().then(function (response) {
+                        console.log("Current History", response.data);
 
-            <Results address={this.state.results} />
+                        console.log("History", response.data);
 
-          </div>
+                        this.setState({history: response.data});
 
-        </div>
+                    }.bind(this));
+                }.bind(this));
+            }
+        }.bind(this));
+    }
+    // This function allows childrens to update the parent.
+    setTerm(term) {
+        this.setState({searchTerm: term});
+    }
+    // Here we render the function
+    render() {
+        return (
+            <div className="container">
+                <div className="row">
+                    <div className="jumbotron">
+                        <h2 className="text-center">Ny Time Article Scrubber</h2>
+                        <p className="text-center">
+                            <em>Search for article of interest</em>
+                        </p>
+                    </div>
 
-        <div className="row">
+                    <div className="col-md-6">
 
-          <History history={this.state.history} />
+                        <Form setTerm={this.setTerm}/>
 
-        </div>
+                    </div>
 
-      </div>
-    );
-  }
-});
+                    <div className="col-md-6">
+
+                        <Results address={this.state.results}/>
+
+                    </div>
+
+                </div>
+
+                <div className="row">
+
+                    <History history={this.state.history}/>
+
+                </div>
+
+            </div>
+        );
+    }
+};
 
 // Export the component back for use in other files
-module.exports = Main;
+export default Main;
--- app/components/children/Form.js
+++ app/components/children/Form.js
@@ -1,72 +1,76 @@
 // Include React
-var React = require("react");
-
+// var React = require("react");
+import React from "react";
 // Creating the Form component
-var Form = React.createClass({
+class Form extends React.Component
+{
+    // Here we set a generic state associated with the text being searched for
+    constructor(props){
+        super(props);
+        this.state = {term: ""};
 
-  // Here we set a generic state associated with the text being searched for
-  getInitialState: function() {
-    return { term: "" };
-  },
+        this.handleChange = this.handleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
 
-  // This function will respond to the user input
-  handleChange: function(event) {
+    // This function will respond to the user input
+    handleChange(event) {
 
-    this.setState({ term: event.target.value });
+        this.setState({term: event.target.value});
 
-  },
+    }
 
-  // When a user submits...
-  handleSubmit: function(event) {
-    // prevent the HTML from trying to submit a form if the user hits "Enter" instead of
-    // clicking the button
-    event.preventDefault();
+    // When a user submits...
+    handleSubmit(event) {
+        // prevent the HTML from trying to submit a form if the user hits "Enter" instead of
+        // clicking the button
+        event.preventDefault();
 
-    // Set the parent to have the search term
-    this.props.setTerm(this.state.term);
-    this.setState({ term: "" });
-  },
-  // Here we describe this component's render method
-  render: function() {
-    return (
-      <div className="panel panel-default">
-        <div className="panel-heading">
-          <h3 className="panel-title text-center">Search</h3>
-        </div>
-        <div className="panel-body text-center">
-          <form onSubmit={this.handleSubmit}>
-            <div className="form-group">
-              <h4 className="">
-                <strong>Title</strong>
-              </h4>
+        // Set the parent to have the search term
+        this.props.setTerm(this.state.term);
+        this.setState({term: ""});
+    }
+    // Here we describe this component's render method
+    render() {
+        return (
+            <div className="panel panel-default">
+                <div className="panel-heading">
+                    <h3 className="panel-title text-center">Search</h3>
+                </div>
+                <div className="panel-body text-center">
+                    <form onSubmit={this.handleSubmit}>
+                        <div className="form-group">
+                            <h4 className="">
+                                <strong>Title</strong>
+                            </h4>
 
-              {/*
-                Note how each of the form elements has an id that matches the state.
-                This is not necessary but it is convenient.
-                Also note how each has an onChange event associated with our handleChange event.
-              */}
-              <input
-                value={this.state.term}
-                type="text"
-                className="form-control text-center"
-                id="term"
-                onChange={this.handleChange}
-                required
-              />
-              <br />
-              <button
-                className="btn btn-primary"
-                type="submit"
-              >
-                Submit
-              </button>
+                            {/*
+                             Note how each of the form elements has an id that matches the state.
+                             This is not necessary but it is convenient.
+                             Also note how each has an onChange event associated with our handleChange event.
+                             */}
+                            <input
+                                value={this.state.term}
+                                type="text"
+                                className="form-control text-center"
+                                id="term"
+                                onChange={this.handleChange}
+                                required
+                            />
+                            <br />
+                            <button
+                                className="btn btn-primary"
+                                type="submit"
+                            >
+                                Submit
+                            </button>
+                        </div>
+                    </form>
+                </div>
             </div>
-          </form>
-        </div>
-      </div>
-    );
-  }
-});
+        );
+    }
+};
 
 // Export the component back for use in other files
-module.exports = Form;
+export default Form;
